4c06adceed0557019d2e00eb9a25adbe010fe14b
Arthur <arthur_wang@adsame.com>
2021-06-30 11:36:11 +0800

    add rever7

--- lc/twosum.go
+++ lc/twosum.go
@@ -1,7 +1,5 @@
 package lc
 
-import "fmt"
-
 func twoSum(nums []int, target int) []int {
 	m := make(map[int]int)
 	for i := 0; i < len(nums); i++ {
@@ -14,11 +12,11 @@ func twoSum(nums []int, target int) []int {
 	return nil
 }
 
-func Test() {
-	nums := []int{2, 7, 11, 15}
-	target := 9
-	res := twoSum(nums, target)
-	if res != nil {
-		fmt.Println(res)
-	}
-}
+// func Test() {
+// 	nums := []int{2, 7, 11, 15}
+// 	target := 9
+// 	res := twoSum(nums, target)
+// 	if res != nil {
+// 		fmt.Println(res)
+// 	}
+// }
--- lcmain.go
+++ lcmain.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"wzq.com/lc"
 )
 
 func main() {
-	fmt.Println("hello lc")
 	lc.Test()
 }
